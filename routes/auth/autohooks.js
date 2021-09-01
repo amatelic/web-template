c2bb4c2d67c24823187c5ef50f303a6be877f1ad
Anže Matelič <amatelic93@gmail.com>
2021-09-01 23:50:36 +0200

    Added authentication routets

--- routes/auth/autohooks.js
+++ routes/auth/autohooks.js
@@ -1,9 +1,28 @@
 
+// basic plugin for checking if user has auth token
 export default async function(instance, opts) {
 	instance.addHook('onRequest',  async function(request, reply) {
 		try {
-			await request.jwtVerify();
+
+			if (request.url === '/auth/generate-token') {
+				if (process.env.NODE_ENV === 'develop') {
+					return true;
+				}	
+			}
+		
+			if (!request.raw.headers.auth) {
+				new Error('Missing token header');
+			}
+		
+			let decoded = await request.jwtVerify();
+
+
+			if (!decoded.user || !decoded.password) {
+				new Error('Token not valid');
+			}
+
 		} catch (err) {
+			console.log('erorrr', err)
 			reply.send(err);
 		}
 	})
--- routes/auth/routes.js
+++ routes/auth/routes.js
@@ -15,6 +15,17 @@ export default async function (fastify, opts) {
 		const token = fastify.jwt.sign({ payload });
 		reply.send({ token });
 	})
+
+	// for testing purposes only 
+	// remove on production and in the autohooks script
+
+	if (process.env.NODE_ENV === 'develop') {
+		fastify.post('/generate-token', async (req, reply) => {
+			const token = await fastify.jwt.sign(req.body);
+			req.log.info(`Created token for user ${req.body.email}`)
+			reply.send({ token });
+		})
+	}
 }
 
 
